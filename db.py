7b6f9c16322b7ee53f0707828bc63bf1567b6b1e
Srinidhi-Janardhana <Srinidhi.Janardhana@education.nsw.gov.au>
2024-12-15 20:43:46 +1100

    Add files via upload

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
+#Adds SQL information to the PWA containing stored information on users and comments
+
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
 def GetDB():
 
-    # Connect to the database and return the connection object
+    # Connect to the database and return the connections
     db = sqlite3.connect(".database/gtg.db")
     db.row_factory = sqlite3.Row
 
@@ -15,4 +17,22 @@ def GetAllComments():
     db = GetDB()
     comments = db.execute("SELECT * FROM Comments").fetchall()
     db.close()
-    return comments
+    return True
+
+def AddComments(user_id, date, game, comments):
+   
+    # Check if any boxes were empty
+    if date is None or game is None:
+        return False
+   
+    # Get the DB and add the comment
+    db = GetDB()
+    db.execute("INSERT INTO Comments(user_id, date, game, comments) VALUES (?, ?, ?, ?)",
+               (user_id, date, game, comments,))
+    db.commit()
+
+ comments = db.execute("""SELECT Comments.date, Comments.game, Comments.score, Users.username
+                            FROM Comments JOIN Users ON Comments.user_id = Users.id
+                            ORDER BY date DESC""").fetchall()
+
+    return GetAllComments
--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+#Imports flask to access the PWA
+
 from flask import Flask, render_template, request
 import db
-
+#Creates Flask
 app = Flask(__name__)
 app.secret_key = "gtg"
-
+#Route to the home page
 @app.route("/")
 def Home():
-    guessData = db.GetAllGuesses()
-    return render_template("index.html", guesses=guessData)
+    commentsData = db.GetAllComments()
+    return render_template("index.html", comments=commentsData)
 
-##################################
-### New code starts here
-##################################
+#Adds a login method for users
 @app.route("/login", methods=["GET", "POST"])
 def Login():
     return render_template("login.html")
-##################################
-### New code ends here
-##################################
+
+#Connects the ratings webpage to the PWA
+@app.route("/add", methods=["GET","POST"])
+def Add():
+ 
+ # Check if they are logged in first
+    if session.get('username') == None:
+        return redirect("/")
+
+    # Has it been submitted?
+    if request.method == "POST":
+        user_id = session['id']
+        date = request.form['date']
+        game = request.form['game']
+        score = request.form['comment']
+
+        # Sends data to add new comments
+        db.AddGuess(user_id, date, game, comment)
+
+    return render_template("add.html")
 
 app.run(debug=True, port=5000)
